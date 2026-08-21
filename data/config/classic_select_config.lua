2480cf0fd490d9eefc688c0dcdee36ae17cfaf82
rENC36 <hellferry5@gmail.com>
2026-08-21 18:10:57 +0300

    design

--- data/config/classic_select_config.lua
+++ data/config/classic_select_config.lua
@@ -12,15 +12,40 @@ M.ATLAS = {
 	FRAME = "btn_lvl_3",
 }
 
-M.COLORS = {
-	COMPLETED_BG      = vmath.vector4(0.20, 0.85, 0.30, 1),
-	COMPLETED_TEXT    = vmath.vector4(1.00, 1.00, 1.00, 1),
-	UNLOCKED_BG       = vmath.vector4(1.00, 1.00, 1.00, 1),
-	UNLOCKED_TEXT     = vmath.vector4(0.00, 0.00, 0.00, 1),
-	LOCKED_BG         = vmath.vector4(0.25, 0.25, 0.25, 1),
-	LOCKED_TEXT       = vmath.vector4(0.50, 0.50, 0.50, 1),
-	CATEGORY_ACTIVE   = vmath.vector4(1.00, 1.00, 1.00, 1),
-	CATEGORY_INACTIVE = vmath.vector4(0.50, 0.50, 0.50, 1),
+M.STATUS = {
+	COMPLETED = {
+		BG_COLOR   = vmath.vector4(0.8, 1.00, 0.8, 1),
+		TEXT_COLOR = vmath.vector4(0.20, 0.20, 0.40, 1),
+		ICON       = "lvl_done_3",
+		ICON_COLOR = vmath.vector4(1, 1, 1, 1),
+	},
+	UNLOCKED = {
+		BG_COLOR   = vmath.vector4(1.00, 1.00, 1.00, 1),
+		TEXT_COLOR = vmath.vector4(0.20, 0.20, 0.40, 1),
+		ICON       = nil,
+		ICON_COLOR = vmath.vector4(1, 1, 1, 1),
+	},
+	LOCKED = {
+		BG_COLOR   = vmath.vector4(0.7, 0.7, 0.7, 1),
+		TEXT_COLOR = vmath.vector4(0.50, 0.50, 0.50, 1),
+		ICON       = "lvl_closed_3",
+		ICON_COLOR = vmath.vector4(1, 1, 1, 0.6),
+	},
+}
+
+M.CATEGORY_COLORS = {
+	easy   = { 
+		active   = vmath.vector4(0.85, 0.98, 0.85, 1), 
+		inactive = vmath.vector4(0.7, 0.7, 0.7, 0.4) 
+	},
+	medium = { 
+		active   = vmath.vector4(0.98, 0.98, 0.80, 1), 
+		inactive = vmath.vector4(0.7, 0.7, 0.7, 0.4) 
+	},
+	hard   = { 
+		active   = vmath.vector4(0.98, 0.85, 0.85, 1), 
+		inactive = vmath.vector4(0.7, 0.7, 0.7, 0.4) 
+	},
 }
 
 M.ANIM = {
